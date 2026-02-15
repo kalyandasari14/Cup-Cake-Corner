1ce408c527f66144db75f26a04dedda72c2dfecd
kalayn <kalyan>
2026-02-15 14:39:09 -0600

    Initial Commit

---
+++ Cup Cake Corner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cup Cake Corner
+//
+//  Created by kalyan on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cup Cake Corner/Cup_Cake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cup_Cake_CornerApp.swift
+//  Cup Cake Corner
+//
+//  Created by kalyan on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Cup_Cake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
