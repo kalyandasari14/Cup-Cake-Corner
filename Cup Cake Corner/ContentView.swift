dba838e4df00b3c797fdb99d73ca89ce74d3ff1e
kalayn <kalyan>
2026-02-15 16:13:56 -0600

    detched an api from the internet

--- Cup Cake Corner/ContentView.swift
+++ Cup Cake Corner/ContentView.swift
@@ -27,7 +27,29 @@ struct ContentView: View {
                 Text(item.trackName).font(.headline)
                 Text(item.collectionName)
             }
+        }.task {
+            await loadData()
         }
+    }
+    
+    func loadData() async{
+        
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do{
+            let(data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
+                results = decodedResponse.results
+            }
+        } catch{
+            print("invalid data")
+        }
+        
+       
+        
     }
 }
 
