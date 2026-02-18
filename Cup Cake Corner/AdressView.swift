2488f317d789dac60bfd09e4aee043ce8aa967c9
kalayn <kalyan>
2026-02-18 13:58:57 -0600

    final project

---
+++ Cup Cake Corner/AdressView.swift
@@ -0,0 +1,18 @@
+//
+//  AdressView.swift
+//  Cup Cake Corner
+//
+//  Created by kalyan on 2/17/26.
+//
+
+import SwiftUI
+
+struct AdressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdressView()
+}
---
+++ Cup Cake Corner/CheckOutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckOutView.swift
+//  Cup Cake Corner
+//
+//  Created by kalyan on 2/18/26.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckOutView()
+}
